0d858e4f27a394a788d135b4c2b88694e2e7665d
李東穎 <dean19990226@gmail.com>
2022-07-02 13:07:27 +0800

    登入頁完成

--- public/js/all.js
+++ public/js/all.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-
+// scoll top
     $('.pagetop').click(function(){
         $('html,body').animate({
          scrollTop:0
         },
          1000);
- });
+ });// scoll end
+
+//  漢堡選單 start
 
     $('.btn-buger').click(function(e){
         $('.nav-content_main').toggleClass('open');
         $('.btn-buger').toggleClass('open');
-    })
+    })//  漢堡選單 end
 
+//點小圖換大圖 start
+$('.sub-product img').click(function(e){
+
+$('.main-product ').attr('src',$(this).attr('src'));
+})
 
 })
